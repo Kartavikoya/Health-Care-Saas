439af384e8a0e995bee99b803b411214b45d31fb
Kartavikoya <www.kartavikoya456@gmail.com>
2026-04-29 11:41:30 +0530

    added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import AppRoutes from './app/routes/AppRoutes.tsx'
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch((error) => {
+      console.error('Service worker registration failed:', error)
+    })
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRoutes />
+    
   </StrictMode>,
+  
 )
